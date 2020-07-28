9ad387ba4601f1053145f3c53cf111d22e83f80b
srajiang <srajiang@gmail.com>
2020-07-28 15:46:49 -0700

    Update apiUtil

--- src/apiUtil.js
+++ src/apiUtil.js
@@ -1,6 +1,12 @@
 const axios = require("axios");
+const AIRTABLE_ENDPOINT = 'https://api.airtable.com/v0/appJ2zDoqvxeNEdQH/Table%201';
+const BEARER_TOKEN = 'keyVv0yLNmlyIb3ze'; // need to protect
 
-export const getData = () => {
-  return axios.get('https://jsonplaceholder.typicode.com/todos/1')
+export const fetchProjects = () => {
+  return axios( { 
+    method: 'get', 
+    url: AIRTABLE_ENDPOINT, 
+    headers: { 'Authorization': BEARER_TOKEN }  
+  });
 }
 
