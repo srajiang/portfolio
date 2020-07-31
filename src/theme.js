00c1bc317a891b71610c29fdbda399882d5399d9
srajiang <srajiang@gmail.com>
2020-07-30 17:52:24 -0700

    Add site themes

--- src/theme.js
+++ src/theme.js
@@ -1,30 +1,51 @@
-// ----------- Colors
+// ---------------------- Colors
 
-export const colors = {
-  
+export const colors_dark = {
+  primary: "#192444",
+  secondary: "#3D4465", // %55 percent
+  accent: "#FF737B", 
+  text_header: "#E3E3E3",
+  text_header_sub: "#908888"
 };
 
-// ----------- Font
+export const colors_light = {
+  primary: "#EEEFF0",
+  secondary: "#E3E5EC",
+  accent: "#FE4F4F",
+  text_header: "#33436F",
+  text_header_sub: "#33436E"
 
+}
+
+// ---------------------- Font
 export const font = {
   weights: fontWeights,
-  family: "",
-  url: ""
+  detail: "Roboto Mono",
+  header: "Raleway",
+  body: "Roboto"
 }
 
 export const fontWeights = {
+  light: 200,
   regular: 400,
   bold: 700,
   extraBold: 800
 }
 
-// ----------- Box Shadows
+export const fontSizes = {
+  header: "4rem",
+  header_sub: "4rem", 
+  body: "2rem",
+  detail: "1rem",
+}
+
+// ---------------------- Box Shadows
 
 
-// ----------- Media Query Helper
-
+// ---------------------- Media Query Helper
 const theme = {
-  colors,
+  colors_light,
+  colors_dark,
   font
 }
 
